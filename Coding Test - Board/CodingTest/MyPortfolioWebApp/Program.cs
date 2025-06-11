131da2aa465b7fec347fff1fc6ef96878747c993
emilianolee <yechan5172@daum.net>
2025-06-11 12:27:05 +0900

    뭔지 모르겠는 커밋

--- Coding Test - Board/CodingTest/MyPortfolioWebApp/Program.cs
+++ Coding Test - Board/CodingTest/MyPortfolioWebApp/Program.cs
@@ -55,6 +55,13 @@ namespace MyPortfolioWebApp
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
             app.Run();
         }
     }
